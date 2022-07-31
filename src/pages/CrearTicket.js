341ed6de9a490685466f46efcb2191d3c4cbc230
Marco Estrada <fericell2909@gmail.com>
2022-07-30 23:57:26 -0500

    create: ticket

--- src/pages/CrearTicket.js
+++ src/pages/CrearTicket.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Row , Col , Typography , Button} from 'antd'
+import { DownloadOutlined } from '@ant-design/icons';
 
 const CrearTicket = () => {
+  
+  const {Title , Text} = Typography;
+
+  const nuevoTicket = () => {
+
+  }
   return (
-    <div>CrearTicket Page</div>
+    <>
+      <Row>
+        <Col span={14} offset={6} align="center">
+          <Title level={3}>
+            Presione el boton para un nuevo Ticket
+          </Title>
+          <Button 
+            type="primary"
+            shape="round"
+            icon={<DownloadOutlined></DownloadOutlined>}
+            size="large"
+            onClick={nuevoTicket}
+          >
+            Nuevo Ticket
+          </Button>
+        </Col>
+      </Row>
+        <Row style={{marginTop: 100}}>
+          <Col span={14} offset={6} align="center">
+            <Text level={2}>
+              Su numero
+            </Text>
+            <br />
+            <Text type="success" style={{fontSize: 55}}>
+              55
+            </Text>
+          </Col>
+        </Row>
+    </>
   )
 }
 
