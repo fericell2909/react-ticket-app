54e14812c025392be6bf69e2c5feac824663995c
Marco Estrada <fericell2909@gmail.com>
2022-07-30 22:48:37 -0500

    routes

--- src/pages/RouterPage.js
+++ src/pages/RouterPage.js
@@ -1,15 +1,42 @@
-import React from 'react'
+import React , {Redirect} from 'react'
+import {BrowserRouter,
+    Routes,
+    Route,
+    Link} from 'react-router-dom'
 import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
     UploadOutlined,
     UserOutlined,
     VideoCameraOutlined,
 } from '@ant-design/icons';
 import { Layout, Menu } from 'antd';
+import Ingresar from './Ingresar';
+import Cola from './Cola';
+import CrearTicket from './CrearTicket';
+import Escritorio from './Escritorio';
 
-const { Sider, Content } = Layout;
+const { Header, Sider, Content } = Layout;
+
+function getItem(label, key, icon, children) {
+    return {
+      key,
+      icon,
+      children,
+      label,
+    };
+  }
+  
+  const items = [
+    getItem(<Link to="/ingresar">Ingresar</Link>, '1', <UserOutlined />),
+    getItem(<Link to="/cola">Cola de tickets</Link>, '2', <UserOutlined />),
+    getItem(<Link to="/crear">Crear Tickets</Link>, '3', <UploadOutlined />),
+  ];
+
 export const RouterPage = () => {
     return (
         <>
+            <BrowserRouter>
             <Layout style={{height: '100vh'}}>
                 <Sider>
                     <div className="logo" />
@@ -17,23 +44,7 @@ export const RouterPage = () => {
                         theme="dark"
                         mode="inline"
                         defaultSelectedKeys={['1']}
-                        items={[
-                            {
-                                key: '1',
-                                icon: <UserOutlined />,
-                                label: 'Ingresar',
-                            },
-                            {
-                                key: '2',
-                                icon: <VideoCameraOutlined />,
-                                label: 'Cola',
-                            },
-                            {
-                                key: '3',
-                                icon: <UploadOutlined />,
-                                label: 'Crear Ticket',
-                            },
-                        ]}
+                        items={items}
                     />
                 </Sider>
                 <Layout className="site-layout">
@@ -45,10 +56,19 @@ export const RouterPage = () => {
                             minHeight: 280,
                         }}
                     >
-                        Content
+                        <Routes>
+                            <Route path="/ingresar" element={<Ingresar />} />
+                            <Route path="/cola" element={<Cola />} />
+                            <Route path="/crear" element={<CrearTicket />} />
+                            
+                            <Route path="/escritorio" element={<Escritorio />} />
+
+                            <Route path="*" element={<>Contenido no Encontrado</>}  />
+                        </Routes>
                     </Content>
                 </Layout>
             </Layout>
+            </BrowserRouter>
         </>
     )
 }
