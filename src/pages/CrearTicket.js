fecc5e9e8bdcf412ba080605718e9c3876c5c757
Marco Estrada <fericell2909@gmail.com>
2022-07-31 21:04:09 -0500

    Crear Ticket

--- src/pages/CrearTicket.js
+++ src/pages/CrearTicket.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import { Row , Col , Typography , Button} from 'antd'
+import React, { useContext , useState} from 'react'
+import { Row, Col, Typography, Button } from 'antd'
 import { DownloadOutlined } from '@ant-design/icons';
 import { useHideMenu } from '../hooks/useHideMenu';
+import { SocketContext } from '../context/SocketContext';
 
-const {Title , Text} = Typography;
+const { Title, Text } = Typography;
 
 const CrearTicket = () => {
-  
+
   useHideMenu(true);
 
-
+  const { socket } = useContext(SocketContext);
+  const [ticket, setTicket] = useState(null)
   const nuevoTicket = () => {
+
+    socket.emit('solicitar-ticket', null, (ticket) => {
+      //console.log(ticket);
+
+      setTicket(ticket);
+    });
 
   }
   return (
@@ -20,7 +28,7 @@ const CrearTicket = () => {
           <Title level={3}>
             Presione el boton para un nuevo Ticket
           </Title>
-          <Button 
+          <Button
             type="primary"
             shape="round"
             icon={<DownloadOutlined></DownloadOutlined>}
@@ -31,17 +39,21 @@ const CrearTicket = () => {
           </Button>
         </Col>
       </Row>
-        <Row style={{marginTop: 100}}>
-          <Col span={14} offset={6} align="center">
-            <Text level={2}>
-              Su numero
-            </Text>
-            <br />
-            <Text type="success" style={{fontSize: 55}}>
-              55
-            </Text>
-          </Col>
-        </Row>
+      {
+        ticket && (
+          <Row style={{ marginTop: 100 }}>
+            <Col span={14} offset={6} align="center">
+              <Text level={2}>
+                Su numero
+              </Text>
+              <br />
+              <Text type="success" style={{ fontSize: 55 }}>
+                {ticket.numero}
+              </Text>
+            </Col>
+          </Row>
+        )
+      }
     </>
   )
 }
